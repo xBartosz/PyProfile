2c9a29f477ba8cd95e1506961f8ffb5dba726458
Bartosz <xbartoszf@gmail.com>
2022-08-02 19:53:20 +0200

    Password reset

--- PyProfile/settings.py
+++ PyProfile/settings.py
@@ -141,5 +141,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'pyprofileofficial@gmail.com'
-EMAIL_HOST_PASSWORD = 'Kozak524$'
+
